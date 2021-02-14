eac0e1089a86b3e1220db835c6a22788f67fb160
david-sanvicente <sanvicented1@gmail.com>
2021-02-14 12:16:22 -0500

    fixed a bug in shopping cart and added new project tile guess my number

--- src/components/tiles/Tiles.tsx
+++ src/components/tiles/Tiles.tsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import pixelate from '../../assets/images/pixelate.png';
 import whackamole from '../../assets/images/whackamole.png';
 import shoppingcart from '../../assets/images/shoppingCart.png';
+import guessNumber from '../../assets/images/guessNumber.png';
 import './styles.scss';
 
 export default class Tiles extends Component {
@@ -71,6 +72,30 @@ export default class Tiles extends Component {
                                 <a href={`https://github.com/david-sanvicente/shoppingcart`} className={`link`}><i className="fas fa-code-branch"></i><span className="highlight">View Source</span></a>
                             </div>
                             <p className={'aside'}>A classic shopping cart application in <span className={`highlight`}>JavaScript</span>. The app renders list items to and from <span className={`highlight`}>collections</span> and makes use of event listeners and input fields. To update the cart, it uses a <span className={`highlight`}>search algorithm</span> using a boolean flag.</p>
+                        </div>
+                        <div className={`project__technologies`}>
+                            <ul className={`project__technologies__list`}>
+                                <li><i className="fas fa-dot-circle"></i><p>HTML</p></li>
+                                <li><i className="fas fa-dot-circle"></i><p>CSS</p></li>
+                                <li><i className="fas fa-dot-circle"></i><p>JavaScript</p></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className={`tile`}>
+                        <h3 className={`heading-secondary`}><span className='highlight'>Guess My Number!</span></h3>
+                        <div className={`project__display`}>
+                            <div className={`project__display__poster`}>
+                                <a href={`https://david-sanvicente.github.io/DOM_practice_guess_the_number/`}>
+                                    <img src={guessNumber} alt={`Project Thumbnail`} />
+                                </a>
+                            </div>
+                            <div className={`project__display__actions`}>
+                                <a href={`https://david-sanvicente.github.io/DOM_practice_guess_the_number/`} className={`link`}><i className="fas fa-desktop"></i><span className="highlight">View Demo</span></a>
+                                {/* <a href={`#`} className={`link link--inactive`}><i className="fas fa-code-branch"></i><span>Source Private</span></a> */}
+                                <a href={`https://github.com/david-sanvicente/DOM_practice_guess_the_number`} className={`link`}><i className="fas fa-code-branch"></i><span className="highlight">View Source</span></a>
+                            </div>
+                            <p className={'aside'}>Another fun game written in <span className={`highlight`}>JavaScript</span>. The game has win and loss <span className={`highlight`}>conditions</span> that update the game's messaging and colors. Demonstrates <span className={`highlight`}>DOM manupulation</span> and <span className={`highlight`}>event handlers</span>.</p>
                         </div>
                         <div className={`project__technologies`}>
                             <ul className={`project__technologies__list`}>
