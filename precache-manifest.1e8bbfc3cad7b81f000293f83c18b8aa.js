5c318dc89b924d3a01e37f2b860ac3d2a2a41242
david-sanvicente <sanvicented1@gmail.com>
2021-04-13 11:05:54 -0400

    Updates

---
+++ precache-manifest.1e8bbfc3cad7b81f000293f83c18b8aa.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "59ce4ea83f071f6575f0",
+    "url": "/david-sanvicente.github.io/static/css/main.840c41e3.chunk.css"
+  },
+  {
+    "revision": "59ce4ea83f071f6575f0",
+    "url": "/david-sanvicente.github.io/static/js/main.f0e847f0.chunk.js"
+  },
+  {
+    "revision": "8d22869e2303ef738a5d",
+    "url": "/david-sanvicente.github.io/static/js/runtime~main.3f877338.js"
+  },
+  {
+    "revision": "e2c944828f1a38eb3cf5",
+    "url": "/david-sanvicente.github.io/static/js/2.f45c1a2b.chunk.js"
+  },
+  {
+    "revision": "16809aa7430829676bea374431cd5308",
+    "url": "/david-sanvicente.github.io/static/media/chai.16809aa7.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/david-sanvicente.github.io/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "43d302c666a0bd3b6d24f4824a639d6f",
+    "url": "/david-sanvicente.github.io/static/media/git.43d302c6.svg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/david-sanvicente.github.io/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "a22a42410e2ce5842380688bac43bd03",
+    "url": "/david-sanvicente.github.io/static/media/nodejs.a22a4241.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/david-sanvicente.github.io/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "f2948be7bf211b1b2828c3aef8720a96",
+    "url": "/david-sanvicente.github.io/static/media/github-2.f2948be7.svg"
+  },
+  {
+    "revision": "32388bba7693ddbd664c04253a756c00",
+    "url": "/david-sanvicente.github.io/static/media/redux.32388bba.svg"
+  },
+  {
+    "revision": "0fb30b49da79da6979871081a23bfd53",
+    "url": "/david-sanvicente.github.io/static/media/express.0fb30b49.svg"
+  },
+  {
+    "revision": "43c9d125de48e59cb29a88dd1b20bc46",
+    "url": "/david-sanvicente.github.io/static/media/sequelize.43c9d125.svg"
+  },
+  {
+    "revision": "dc8c590334565b163ce4ac438de917af",
+    "url": "/david-sanvicente.github.io/static/media/sprayrbg.dc8c5903.jpg"
+  },
+  {
+    "revision": "80629b9f9b8881a4251b9cd1555a6447",
+    "url": "/david-sanvicente.github.io/static/media/botbuddiesbg.80629b9f.jpg"
+  },
+  {
+    "revision": "f5399af25c8f18ba5fbf86e51ae94de7",
+    "url": "/david-sanvicente.github.io/static/media/pixelate.f5399af2.png"
+  },
+  {
+    "revision": "a24c0a455fb037b29401f12cb3fdb313",
+    "url": "/david-sanvicente.github.io/static/media/whackamole.a24c0a45.png"
+  },
+  {
+    "revision": "0b9de2eeaf16b5dbdc25dbae89591b8f",
+    "url": "/david-sanvicente.github.io/static/media/shoppingCart.0b9de2ee.png"
+  },
+  {
+    "revision": "9a2f35e563b527ba9e582f7eba67a2e7",
+    "url": "/david-sanvicente.github.io/static/media/guessNumber.9a2f35e5.png"
+  },
+  {
+    "revision": "11122160c1ddddd08582be752aebe4f1",
+    "url": "/david-sanvicente.github.io/static/media/nybright.11122160.jpg"
+  },
+  {
+    "revision": "5744307222aab37a8b1c2781be153948",
+    "url": "/david-sanvicente.github.io/index.html"
+  }
+];
